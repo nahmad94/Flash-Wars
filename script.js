380f270e177c1c7e59dd6e6181de43b3b5188ac2
nahmad94 <deevan11@gmail.com>
2022-10-26 12:04:57 -0400

    limits flip to one side and adds counter

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ var modal = document.querySelector('#myModal')
 var howToBut = document.querySelector('#howTo')
 var close = document.querySelector('.close')
 
+
+let boardContainer = document.querySelector('.board')
+let cardContainer = document.querySelector('.cards')
+let cards = document.querySelectorAll('.cards_single');
+let moves = document.querySelector('.moves')
+let timer = document.querySelector('.timer')
+let start = document.querySelector('.start')
+let win = document.querySelector('.win')
+let flippedCards = 0
+let totalFlips = 0
+
+const initial = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
+
 howToBut.onclick = function() {
     modal.style.display = 'block'
 }
@@ -16,16 +35,77 @@ window.onclick = function(event) {
     }
 }
 
-var cards = document.querySelectorAll('.cards_single');
 
-function flip() {
-    this.classList.toggle('flip')
-    console.log('flipped')
+
+
+
+
+
+
+
+
+
+
+
+
+cards.forEach((card) => card.addEventListener('click', function() {
+    console.log(this)
+    if (this.classList.contains('flip') === true) {
+        console.log('already flipped')
+        
+    } else if (this.classList.contains('flip') === false) {
+        this.classList.toggle('flip')
+        flippedCards++
+    }
+}));
+
+
+
+
+
+function flip(event) {
+    if (this.classList('flip') === true) {
+        console.log('already flipped')
+    } else if (this.classList('flip') === false) {
+        this.classList.toggle('flip')
+        flippedCards++
+    }
+
+
+
+    // this.classList.toggle('flip')
+    flippedCards++
+    totalFlips++
+    
+    // const eventTarget = event.target
+    // const eventParent = eventTarget.parentElement
+    // console.log(eventParent)
+
+    // if (eventTarget.className.includes('card') && !eventParent.className.includes('flip')) {
+    //     flipCard(eventParent)
+    // } else {
+    //     console.log('error')
+    // }
 };
 
-cards.forEach((card) => card.addEventListener('click', flip));
+start.addEventListener('click', initial)
 
 
+
+
+
+
+
+
+
+
+// document.body.onload = startGame();
+
+// function startGame() {
+//     for (let i = cards.children.length; i >= 0; i--) {
+//         cards.appendChild(cards.children[Math.random() * i | 0])
+//     }
+// }
 
 
 
